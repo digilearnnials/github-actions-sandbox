ae4887e8938287a6324256ffb0c85aa8f9264d22
Ignacio Mosconi <nacho@digilearnnials.com>
2023-04-11 12:50:09 -0300

    Refactor clone deployment repository script

--- .github/scripts/deployment-repository/clone-deployment-repository.sh
+++ .github/scripts/deployment-repository/clone-deployment-repository.sh
@@ -1,15 +1,9 @@
 repositoryUrl=$DEPLOYMENT_REPO_URL
 
 usernamePlaceholder="<username>"
-actualUsername=$DEPLOYMENT_REPO_USERNAME
-
-repositoryUrl=$(echo $repositoryUrl | sed "s/$usernamePlaceholder/$actualUsername/g")
-
 appPasswordPlaceholder="<app-password>"
-actualAppPassword=$DEPLOYMENT_REPO_APP_PASSWORD
 
-repositoryUrl=$(echo $repositoryUrl | sed "s/$appPasswordPlaceholder/$actualAppPassword/g")
-
-echo $repositoryUrl
+repositoryUrl=$(echo $repositoryUrl | sed "s/$usernamePlaceholder/$DEPLOYMENT_REPO_USERNAME/g")
+repositoryUrl=$(echo $repositoryUrl | sed "s/$appPasswordPlaceholder/$DEPLOYMENT_REPO_APP_PASSWORD/g")
 
 git clone $repositoryUrl deployment
