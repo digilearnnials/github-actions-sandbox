c6e44717098f0274625dfb1a34454bc258ada7ad
Ignacio Mosconi <nacho@digilearnnials.com>
2023-04-17 13:02:40 -0300

    Fix Docker login command for build docker image bash script

--- .github/scripts/playfab/build-docker-image.sh
+++ .github/scripts/playfab/build-docker-image.sh
@@ -4,6 +4,6 @@ cp -R "build/$BUILD_TARGET/"* Docker/bin
 
 cd "Docker" || exit
 
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD $DOCKER_SERVER_URL"
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD" "$DOCKER_SERVER_URL"
 docker build -t "$DOCKER_SERVER_URL/$DOCKER_IMAGE_NAME:$TAG_NAME" .
 docker push "$DOCKER_SERVER_URL/$DOCKER_IMAGE_NAME:$TAG_NAME"
