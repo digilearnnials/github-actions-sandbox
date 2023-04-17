23d017317d356da855cb4c78182e2a2df3092144
Ignacio Mosconi <nacho@digilearnnials.com>
2023-04-17 14:25:55 -0300

    Update environment detection so that all environments have a specific suffix inside semantic versioning

--- .github/scripts/common/detect-environment.sh
+++ .github/scripts/common/detect-environment.sh
@@ -2,19 +2,19 @@
 
 environment=""
 
-currentVersion=$VERSION_NAME
 currentVersionParts=()
 
-readarray -d - -t currentVersionParts <<< "$currentVersion"
+readarray -d - -t currentVersionParts <<< "$VERSION_NAME"
 
-if [ ${#currentVersionParts[@]} == 1 ]
+if [ "${currentVersionParts[1]}" == "dev" ]
 then
-    environment="production"
+    environment="development"
 elif [ "${currentVersionParts[1]}" == "test" ]
 then
     environment="staging"
-else
-    environment="development"
+elif [ "${currentVersionParts[1]}" == "prod" ]
+then
+    environment="production"
 fi
 
 echo "targetEnvironment=$environment" >> "$GITHUB_OUTPUT"
