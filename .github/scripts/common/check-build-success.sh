75c44f2ca48f3d055f29d5e15a15285e264ab00d
Ignacio Mosconi <nacho@digilearnnials.com>
2023-05-29 18:32:00 -0300

    Add check build success script to avoid performing post build steps if the output was not generated properly

---
+++ .github/scripts/common/check-build-success.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+buildSucceeded=false
+
+if [ -d "build/$BUILD_TARGET" ]
+then
+    echo "Success!"
+    buildSucceeded=true;
+else
+    echo "Failure!"
+    buildSucceeded=false;
+fi
+
+ echo "buildSucceeded=$buildSucceeded" >> "$GITHUB_OUTPUT"
