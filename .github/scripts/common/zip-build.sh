47c7a00a5ae8d33841d014aa0829b4b2745e97d3
Ignacio Mosconi <nacho@digilearnnials.com>
2023-06-22 15:56:09 -0300

    Fix Zip Build script to work for WebGL and other platforms

--- .github/scripts/common/zip-build.sh
+++ .github/scripts/common/zip-build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+zipFolder=""
+
 mkdir release
 
 if [ "$BUILD_TARGET" = "WebGL" ]
 then
     cp -R "build/$BUILD_TARGET/$BUILD_FULL_NAME" release
+    zipFolder=$BUILD_FULL_NAME
 else
     cp -R "build/$BUILD_TARGET" release
+    zipFolder=$BUILD_TARGET
 fi
 
 cd release || exit
@@ -15,6 +19,6 @@ oldStr=" "
 newStr="_"
 zipName=${BUILD_FULL_NAME//$oldStr/$newStr}
 
-zip -r "$zipName.zip" "$BUILD_FULL_NAME"
+zip -r "$zipName.zip" "$zipFolder"
 
 echo "targetZipName=$zipName" >> "$GITHUB_OUTPUT"
